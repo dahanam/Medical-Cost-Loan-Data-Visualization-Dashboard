fe36bcf920921c2b6978460d326cd27d334705cc
Dahana Moz Ruiz <115283349+dahanam@users.noreply.github.com>
2025-04-23 00:37:06 -0400

    Add files via upload

---
+++ dbconfig.php
@@ -0,0 +1,6 @@
+<?php
+$hostname = "imc.kean.edu";
+$username = "mozruizd";
+$password = "1178488";
+$dbname = "2024F_mozruizd";
+?>
---
+++ dbconfigloan.php
@@ -0,0 +1,7 @@
+<?php
+$hostname = "imc.kean.edu";
+$username = "gonzasa3";
+$password = "1182658";
+$database = "2024F_gonzasa3";
+//'gonzasa3', '1182658', '2024F_gonzasa3'
+?>
